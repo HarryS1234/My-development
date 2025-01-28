6cac4c94c5e22945541338e9d8801b5feb61ca86
HarryS1234 <158245990+HarryS1234@users.noreply.github.com>
2025-01-28 18:52:21 -0500

    new changes

--- src/pages/Refrigeration.jsx
+++ src/pages/Refrigeration.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import Header from '../components/ReuseComponents/Header'
-import Navbar from '../components/ReuseComponents/Navbar'
-import Ref1 from '../components/RefComponents/Ref1'
-import FlexboxSection from '../components/RefComponents/FlexBoxSection'
-import AssistanceSection from '../components/RefComponents/AssistanceSection'
-import Footer from '../components/ReuseComponents/Footer.jsx'
+import React from "react";
+import Header from "../components/ReuseComponents/Header.jsx";
+import Navbar from "../components/ReuseComponents/Navbar.jsx";
+import Ref1 from "../components/RefComponents/Ref1";
 
+import FlexBoxSection from "../components/RefComponents/FlexboxSection.jsx";
+
+
+import AssistanceSection from "../components/RefComponents/AssistanceSection";
+import Footer from "../components/ReuseComponents/Footer.jsx";
 
 const Refrigeration = () => {
   return (
     <div>
-
-        <Header/>
-        <Navbar/>
-        <Ref1/>
-        <FlexboxSection/>
-        <AssistanceSection/>
-        <Footer/>
+      <Header />
+      <Navbar />
+      <Ref1 />
+      <FlexBoxSection />
+      <AssistanceSection />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Refrigeration
+export default Refrigeration;
